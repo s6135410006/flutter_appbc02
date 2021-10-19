f4a7e36f8b17f8914455417fa53e97cbe872c71d
s6135410006 <s6135410006@sau.ac.th>
2021-10-19 22:08:05 +0700

    V.1.2 :

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_appbc_02/views/login_ui.dart';
+import 'package:flutter_appbc_02/views/sign_up_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUI extends StatelessWidget {
   const HomeUI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //โค้ดเซ็ตค่า แบบเต็มจอ
+    //code for set value of Status Bar to Fullscreen indeed.
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle.light.copyWith(
-        statusBarBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
         statusBarColor: Colors.transparent,
       ),
     );
@@ -20,10 +23,110 @@ class HomeUI extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(
-                height: 50.0,
-              ),  
-            
-
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 60,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      FontAwesomeIcons.headphones,
+                      size: 80,
+                      color: Color(0xFF444444),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 60.0,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "Start\n" + "Your\n" + "Adventure",
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF444444),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LogInUI();
+                      },
+                    ),
+                  );
+                },
+                child: Text(
+                  "Log in",
+                  style: TextStyle(
+                    fontFamily: "Kanit",
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                style: OutlinedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 120,
+                    50,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  side: BorderSide(
+                    color: Colors.tealAccent,
+                  ),
+                  backgroundColor: Colors.teal,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "You don't have an account? ",
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return SignUpUI();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Sign up here",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
